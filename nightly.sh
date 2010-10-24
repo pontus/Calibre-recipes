d2191611b6a23c1392f79733d2997e3f677a77be
Pontus Freyhult <pontus_github@soua.net>
2010-10-24 21:40:47 +0200

    Fix mount

--- nightly.sh
+++ nightly.sh
@@ -2,14 +2,19 @@
 
 cd "`dirname $0`"
 fmt=.mobi
-base=/media/Kindle
+base=/tmp/Kindle$$
+sudo rm -rf  "$base"
+sudo mkdir "$base"
 
-sudo umount "$base"
-sudo mount "$base" -o user=pontus
+sudo umount /dev/disk/by-label/Kindle
+
+sudo mount /dev/disk/by-label/Kindle "$base" -o uid=`id -u`
 
 for p in *recipe; do 
-  rm "$base/dagens_${p%.recipe}.$fmt"
-  ebook-convert ${p%.recipe} $base/dagens_${p%.recipe}.$fmt"
+  rm "$base/dagens_${p%.recipe}$fmt"
+  ebook-convert "$p" "$base/dagens_${p%.recipe}$fmt"
+
 done
 
 sudo umount "$base"
+sudo rm -rf "$base"
