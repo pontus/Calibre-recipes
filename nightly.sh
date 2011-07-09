7423f48a8892439fd4d5e44742541b561e94fdec
Pontus Freyhult <pontus_github@soua.net>
2011-07-09 13:26:51 +0200

    Use nook STR instead of Kindle now.

--- nightly.sh
+++ nightly.sh
@@ -6,17 +6,45 @@ ping -c 3 www.google.com || exit 1
 cd "`dirname $0`"
 git pull
 
-fmt=.mobi
-base=/media/Kindle
-docbase=$base/documents/tidningar
+fmt=.epub
+docbase=/tmp/calibrecip$$
+remotebase="/media/My Files/Newspapers/"
 
-umount "$base"
-mount "$base"
+mkdir -p "$docbase"
 
 for p in *recipe; do 
-  rm "$docbase/dagens_${p%.recipe}$fmt"
-  ebook-convert "$p" "$docbase/dagens_${p%.recipe}$fmt"
-
+  rm -rf "$docbase/dagens_${p%.recipe}$fmt"
+  ebook-convert "$p" "$docbase/dagens_${p%.recipe}$fmt" --output-profile=nook --prefer-metadata-cover
 done
 
-umount "$base"
+done=no
+
+cd "$docbase"
+runs=0
+
+while [ "x$done" != xyes ]; do
+ 
+  if /usr/local/android/android-sdk-linux_x86/platform-tools/adb connect 192.168.15.143:5555; then
+    tr=yes
+    for p in *; do
+      if /usr/local/android/android-sdk-linux_x86/platform-tools/adb push "$p" "$remotebase"; then
+        :
+      else
+        tr=no
+      fi
+    done
+
+    done="$tr"
+  else
+    sleep 15
+  fi
+
+  runs=$((runs+1))
+  if [ "$runs" -gt 5000 ]; then
+    # give up after 5000 rounds
+    done=yes
+  fi
+done
+
+rm -rf "$docbase"
+
